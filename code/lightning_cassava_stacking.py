afeafbe04ecc61c17aaeabee16199783369e9cbc
riron1206 <rironyokoi@gmail.com>
2021-01-20 16:42:53 +0900

    add py from my_PC

---
+++ code/lightning_cassava_stacking.py
@@ -0,0 +1,221 @@
+import os
+import sys
+import yaml
+import numpy as np
+import pandas as pd
+
+import pickle
+from sklearn.metrics import accuracy_score, log_loss
+from colorama import Fore
+
+y_ = Fore.YELLOW
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.utils.data import DataLoader, Dataset
+from torch.cuda import amp
+
+import pytorch_lightning as pl
+from pytorch_lightning.metrics.functional import accuracy
+from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+if os.getcwd() in ["/kaggle/working", "/content"]:
+    sys.path.append("./kaggle_Cassava/code")
+else:
+    sys.path.append(r"C:\Users\81908\MyGitHub\kaggle_Cassava\code")
+
+from smooth_ce_loss import SmoothCrossEntropyLoss
+from bi_tempered_loss import BiTemperedLoss
+from pytorch_stacking import (
+    MLP,
+    CNNStacking1d,
+    CNNStacking2d,
+    GCNStacking,
+    set_random_seed,
+)
+
+
+class StackingDatasetMLP(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __getitem__(self, index):
+        x = self.x[index]
+        y = self.y[index]
+
+        ## 過学習防ぐためにラベルにガウシアンノイズ加算
+        ## https://www.kaggle.com/c/stanford-covid-vaccine/discussion/189709
+        # if noise_scale > 0.0:
+        #    y = y + np.random.normal(0.0, scale=noise_scale)
+
+        return {"x": x, "y": y}
+
+    def __len__(self):
+        return len(self.y)
+
+
+class StackingDatasetCNN(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.reset_model_order()
+
+    def reset_model_order(self):
+        self.model_order = np.arange(self.x.shape[-1])
+
+    def shuffle_model_order(self, seed):
+        np.random.seed(seed)
+        self.model_order = np.random.permutation(self.model_order)
+
+    def __getitem__(self, index):
+        x = self.x[index][..., self.model_order]
+        y = self.y[index]
+
+        ## 過学習防ぐためにラベルにガウシアンノイズ加算
+        ## https://www.kaggle.com/c/stanford-covid-vaccine/discussion/189709
+        # if noise_scale > 0.0:
+        #    y = y + np.random.normal(0.0, scale=noise_scale)
+
+        return {"x": x, "y": y}
+
+    def __len__(self):
+        return len(self.y)
+
+
+class StackingDataModule(pl.LightningDataModule):
+    def __init__(
+        self, x_train, x_valid, y_train, y_valid, CFG, stacking_type="mlp", tmp_seed=0
+    ):
+        super().__init__()
+        self.x_train = x_train
+        self.x_valid = x_valid
+        self.y_train = y_train
+        self.y_valid = y_valid
+        self.CFG = CFG
+        self.stacking_type = stacking_type
+        self.tmp_seed = tmp_seed
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, stage=None):
+        if self.stacking_type == "mlp":
+            self.train_dataset = StackingDatasetMLP(self.x_train, self.y_train)
+            self.val_dataset = StackingDatasetMLP(self.x_valid, self.y_valid)
+        else:
+            self.train_dataset = StackingDatasetCNN(self.x_train, self.y_train)
+            self.val_dataset = StackingDatasetCNN(self.x_valid, self.y_valid)
+
+            # shffule order モデルの並び順が性能に影響するので順番入れ替え
+            self.train_dataset.reset_model_order()
+            self.train_dataset.shuffle_model_order(self.tmp_seed)
+            self.val_dataset.reset_model_order()
+            self.val_dataset.shuffle_model_order(self.tmp_seed)
+
+    def train_dataloader(self):
+        train_loader = DataLoader(
+            self.train_dataset,
+            batch_size=self.CFG.batch_size,
+            shuffle=self.CFG.shuffle,
+            num_workers=self.CFG.num_workers,
+        )
+        print(f"train_loader: {len(train_loader)}")
+        return train_loader
+
+    def val_dataloader(self):
+        val_loader = DataLoader(
+            self.val_dataset,
+            batch_size=self.CFG.batch_size,
+            shuffle=False,
+            num_workers=self.CFG.num_workers,
+        )
+        print(f"val_loader: {len(val_loader)}")
+        return val_loader
+
+
+class LitStackingModel(pl.LightningModule):
+    def __init__(self, CFG):
+        super().__init__()
+        self.CFG = CFG
+
+        self.use_amp = True  # apex amp を有効にする(16-bit mixed precision) https://pytorch-lightning.readthedocs.io/en/stable/lightning_module.html
+
+        if "mlp" in CFG.arch:
+            self.net = MLP(**CFG.mlp_params)
+        elif "cnmn1d" in CFG.arch:
+            self.net = CNNStacking1d(**CFG.cnn1d_params)
+        elif "cnmn2d" in CFG.arch:
+            self.net = CNNStacking2d(**CFG.cnn2d_params)
+
+    def forward(self, x):
+        out = self.net(x)
+        return out
+
+    def configure_optimizers(self):
+        if self.CFG.opt == "adam":
+            optimizer = torch.optim.Adam(
+                self.parameters(),
+                lr=self.CFG.lr,
+                weight_decay=self.CFG.weight_decay,
+                amsgrad=False,
+            )
+        else:
+            optimizer = torch.optim.AdamW(
+                self.parameters(), lr=self.CFG.lr, weight_decay=self.CFG.weight_decay
+            )
+
+        if self.CFG.lr_scheduler == "CosineAnnealingLR":
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer, T_max=self.CFG.T_max, eta_min=self.CFG.min_lr
+            )
+        elif self.CFG.lr_scheduler == "CosineAnnealingWarmRestarts":
+            scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
+                optimizer, T_0=self.CFG.T_max, T_mult=1, eta_min=self.CFG.min_lr
+            )
+        else:
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer, step_size=2, gamma=0.1
+            )
+
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch["x"].float(), batch["y"]
+
+        if self.CFG.train_loss_name == "SmoothCrossEntropyLoss":
+            loss_fn = SmoothCrossEntropyLoss(smoothing=self.CFG.smoothing).to(
+                self.CFG.device
+            )
+        elif self.CFG.train_loss_name == "BiTemperedLoss":
+            loss_fn = BiTemperedLoss(
+                t1=self.CFG.t1, t2=self.CFG.t2, smoothing=self.CFG.smoothing
+            ).to(self.CFG.device)
+        else:
+            loss_fn = nn.CrossEntropyLoss().to(self.CFG.device)
+
+        y_hat = self(x.float())
+        loss = loss_fn(y_hat, y)
+
+        acc = accuracy(y_hat, y)
+        self.log(
+            "train_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True
+        )
+        self.log(
+            "train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True
+        )
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch["x"].float(), batch["y"]
+        y_hat = self(x)
+        loss = nn.CrossEntropyLoss()(y_hat, y)
+        acc = accuracy(y_hat, y)
+        self.log("val_acc", acc, prog_bar=True, logger=True),
+        self.log("val_loss", loss, prog_bar=True, logger=True)
---
+++ code/mix_aug_table.py
@@ -0,0 +1,62 @@
+# https://qiita.com/Y_oHr_N/items/7d2a8ca320f3e658b200
+
+import random as rn
+
+import numpy as np
+from sklearn.utils import check_random_state
+
+
+def mixup(x, y=None, alpha=0.2, p=1.0, random_state=None):
+    n, _ = x.shape
+
+    if n is not None and rn.random() < p:
+        random_state = check_random_state(random_state)
+        l = random_state.beta(alpha, alpha)
+        shuffle = random_state.choice(n, n, replace=False)
+
+        x = l * x + (1.0 - l) * x[shuffle]
+
+        if y is not None:
+            y = l * y + (1.0 - l) * y[shuffle]
+
+    return x, y
+
+
+def cutmix(x, y=None, alpha=1.0, p=1.0, random_state=None):
+    n, h, w, _ = x.shape
+
+    if n is not None and rn.random() < p:
+        random_state = check_random_state(random_state)
+        l = np.random.beta(alpha, alpha)
+        r_h = int(h * np.sqrt(1.0 - l))
+        r_w = int(w * np.sqrt(1.0 - l))
+        x1 = np.random.randint(h - r_h)
+        y1 = np.random.randint(w - r_w)
+        x2 = x1 + r_h
+        y2 = y1 + r_w
+        shuffle = random_state.choice(n, n, replace=False)
+
+        x[:, x1:x2, y1:y2] = x[shuffle, x1:x2, y1:y2]
+
+        if y is not None:
+            y = l * y + (1.0 - l) * y[shuffle]
+
+    return x, y
+
+
+def cutmix_for_tabular(x, y=None, alpha=1.0, p=1.0, random_state=None):
+    n, d = x.shape
+
+    if n is not None and rn.random() < p:
+        random_state = check_random_state(random_state)
+        l = random_state.beta(alpha, alpha)
+        mask = random_state.choice([False, True], size=d, p=[l, 1.0 - l])
+        mask = np.where(mask)[0]
+        shuffle = random_state.choice(n, n, replace=False)
+
+        x[:, mask] = x[shuffle, mask]
+
+        if y is not None:
+            y = l * y + (1.0 - l) * y[shuffle]
+
+    return x, y
