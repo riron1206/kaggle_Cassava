ec72f05595afbb5061124ac80e32626479a9beb0
riron1206 <rironyokoi@gmail.com>
2021-01-22 20:13:24 +0900

    add py from my_PC

--- code/util_torch.py
+++ code/util_torch.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
 import torch
 import torch.nn as nn
 
@@ -29,3 +32,36 @@ def freeze_bn(net):
     except ValuError:
         print("error with batchnorm2d or layernorm")
         return
+
+
+def torchvision_joint(images, nrow=2, padding=0, is_show=False):
+    """画像をただ並べる
+    https://blog.shikoan.com/torchvision-image-tile/
+    Usage:
+        # 2x2のカラー画像を作る
+        def create_color_images(n=4):
+            images = np.zeros((n, 64, 64, 3), np.uint8)  # NHWCのフォーマット
+            # 0-2 = Red, Green, Blue
+            images[0, :, :, 0] = 255
+            images[1, :, :, 1] = 255
+            images[2, :, :, 2] = 255
+            # 3-5 = Yellow, Aqua, Fuchsia
+            images[3, :, :, [0, 1]] = 255
+            return images
+        images = create_color_images(4)
+        joined_images = torchvision_joint(images, nrow=2, padding=0, is_show=True)
+    """
+    images = np.transpose(images, [0, 3, 1, 2])  # PyTorch用に, NHWC -> NCHW に変換
+    # PyTorchのテンソルにする（Numpy配列から作る場合はtorch.Tensorよりas_tensorのほうが良い）
+    images_tensor = torch.as_tensor(images)
+    # 一つの画像に統合
+    # paddingの値→隙間, nrow=行あたりの画像数（端数は埋められる）
+    joined_images_tensor = torchvision.utils.make_grid(
+        images_tensor, nrow=nrow, padding=padding
+    )
+    joined_images = joined_images_tensor.numpy()  # PyTorchのテンソル→Numpy配列
+    joined_images = np.transpose(joined_images, [1, 2, 0])  # NCHW -> NHWCに変換
+    if is_show:
+        plt.imshow(joined_images)
+        plt.show()
+    return joined_images
